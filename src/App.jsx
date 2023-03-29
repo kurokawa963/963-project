4fe4e61f47c4c04ede43f89048cda3d9da3060b3
kurokawa963 <ttrknk115@outlook.jp>
2023-03-29 23:32:34 +0900

    地図入れたい

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ import { Mypage } from "./pages/Mypage";
 import { Choice } from "./pages/Choice";
 import { Making } from "./pages/Making";
 import { Playing } from "./pages/Playing";
+import { Clear } from "./pages/Clear";
+import { Archives } from "./pages/Archives";
+
+import kallomate from "../img/kallomate.png"
 
 
 const App = () => {
@@ -19,6 +23,7 @@ const App = () => {
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+
     });
   }, []);
 
@@ -36,17 +41,20 @@ const App = () => {
           <header class="text-gray-600 body-font">
             <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
               <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                <Link to="/"><svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
-                  <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                </svg></Link>
+                <Link to="/">
+                  {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2" > */}
+                  <img src={kallomate}  className="w-12 h-12" />
+                  {/* <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path> */}
+                  {/* </svg> */}
+                </Link>
                 <span class="ml-3 text-xl">
                   <Link to="/">過労☆メイト</Link></span>
               </a>
               <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                <Link to="/Login" class="mr-5 hover:text-gray-900">ログイン</Link>
-                <Link to="/Register" class="mr-5 hover:text-gray-900">新規登録</Link>
-                <Link to="" class="mr-5 hover:text-gray-900"></Link>
-                <Link to="" class="mr-5 hover:text-gray-900"></Link>
+                <Link to="/Login" class="mx-2 hover:text-gray-900">ログイン</Link>
+                <Link to="/Register" class="mx-2 hover:text-gray-900">新規登録</Link>
+                {/* <Link to="" class="mr-5 hover:text-gray-900"></Link>
+                <Link to="" class="mr-5 hover:text-gray-900"></Link> */}
               </nav>
             </div>
           </header>
@@ -82,15 +90,20 @@ const App = () => {
             <header class="text-gray-600 body-font">
               <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                  <Link to="/"><svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
+                    <Link to="/">
+                      <img src={kallomate} className="w-12 h-12" />
+
+                      {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                  </svg></Link>
+                      </svg> */}
+                    </Link>
                   <span class="ml-3 text-xl">
                     <Link to="/">過労☆メイト</Link></span>
                 </a>
                 <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
                     <Link to="/choice" class="mr-5 hover:text-indigo-500">スタンプラリーで遊ぶ</Link>
                     <Link to="/making" class="mr-5 hover:text-indigo-500">スタンプラリーを作る</Link>
+                    <Link to={`/archives/${user.uid}`} class="mr-5 hover:text-indigo-500">旅の記録</Link>
                   <Link to="/mypage" class="mr-5 hover:text-indigo-500">マイページ</Link>
                     <Link to="/" class="mr-5 hover:text-indigo-500" onClick={logout}>ログアウト</Link>
                 </nav>
@@ -118,6 +131,8 @@ const App = () => {
               <Route path="/choice" element={<Choice />}></Route>
               <Route path="/making" element={<Making />}></Route>
               <Route path="/playing/:id" element={<Playing />} />
+                <Route path="/clear" element={<Clear />} />
+                <Route path="/archives/:id" element={<Archives />}/>
 
             </Routes>
 
